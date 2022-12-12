cc0cef06c0cc04e7c915121db824d8be04b65044
Reinaldo <reinaldoper83@gmail.com>
2022-12-12 11:48:54 -0300

    ajuste

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
+/* const morgan = require('morgan'); */
 
 const employee = require('./controllers/employee.controller');
 
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(morgan('tiny'));
+/* app.use(morgan('tiny')); */
 
 app.get('/employees', employee.getAll);
 
